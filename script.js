cb171437d10a62e1e05e22376a7caec75ee5d8fa
sumodkumarchaudhary63-sudo <sumodkumarchaudhary63@gmail.com>
2026-01-04 18:59:46 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -62,4 +62,9 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+    // Set Dynamic Year
+    const yearSpan = document.getElementById('current-year');
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
 });
